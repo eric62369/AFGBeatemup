95a7f31f0a5c2247ededa5651ba8357720592f94
Eric Yoon <yoone2@cs.washington.edu>
2020-09-23 14:55:48 -0700

    Better forcebox system

--- Assets/Scripts/Player/PushOutEnemy.cs
+++ Assets/Scripts/Player/PushOutEnemy.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class PushOutEnemy : MonoBehaviour
 {
     private PlayerMovementController playerMovement;
-    
+    private Collider2D enemyPushOutTrigger;
+    private Collider2D enemyPushOutBox;
     void Start()
     {
         playerMovement = this.transform.root.gameObject.GetComponent<PlayerMovementController>();
+        Component[] pushboxes = GetComponents(typeof(Collider2D));
+        enemyPushOutTrigger = (Collider2D) pushboxes[0];
+        enemyPushOutBox = (Collider2D) pushboxes[1];
     }
     private void OnTriggerStay2D(Collider2D other)
     {
         if (playerMovement.isGrounded && other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            
+            Debug.Log("WOW!");
+            enemyPushOutBox.enabled = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            Debug.Log("OWO!");
+            enemyPushOutBox.enabled = false;
         }
     }
 }
