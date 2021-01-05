5abc025e3248907422454d032f6d7ba8dd69e50d
Eric Yoon <yoone2@cs.washington.edu>
2021-01-05 14:01:31 -0800

    new pushbox solution

--- Assets/Scripts/Player/PushOutEnemy.cs
+++ Assets/Scripts/Player/PushOutEnemy.cs
@@ -7,24 +7,27 @@ public class PushOutEnemy : MonoBehaviour
 {
     // private PlayerMovementController playerMovement;
     private Transform selfTransform;
-    // private Rigidbody2D rb2d;
+    private Rigidbody2D rb2d;
     private Collider2D pushOutTrigger;
 
     void Start()
     {
         // playerMovement = this.transform.root.gameObject.GetComponent<PlayerMovementController>();
         selfTransform = this.transform.root.gameObject.transform;
-        // rb2d = this.transform.root.gameObject.GetComponent<Rigidbody2D>();
+        rb2d = this.transform.root.gameObject.GetComponent<Rigidbody2D>();
         pushOutTrigger = GetComponent<Collider2D>();
     }
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Pushbox") || other.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Pushbox"))
         {
             // repel pushboxes fast
-            Debug.Log("PushOut");
-            // rb2d.AddForce(new Vector2(-2.0f, 0f), ForceMode2D.Impulse);
-            selfTransform.Translate(CalculatePushOutVector(pushOutTrigger, other));
+            Vector3 pushVector = CalculatePushOutVector(pushOutTrigger, other);
+            selfTransform.Translate(pushVector);
+            // rb2d.AddForce(new Vector2(pushVector.x * 2, 0f), ForceMode2D.Impulse);
+        } else if (other.gameObject.layer == LayerMask.NameToLayer("Wall")) {
+            Vector3 pushVector = CalculatePushOutVector(pushOutTrigger, other);
+            selfTransform.Translate(pushVector * 2);
         }
     }
     // private void OnTriggerExit2D(Collider2D other)
@@ -39,7 +42,6 @@ public class PushOutEnemy : MonoBehaviour
         Vector3 otherPosition = other.transform.position;
         // Debug.Log("self: " + selfPosition.x);
         // Debug.Log("other: " + otherPosition.x);
-        Debug.Log("self - other: " + (selfPosition.x - otherPosition.x));
         Vector2 selfBoxSize = ((BoxCollider2D) self).size;
         Vector2 otherBoxSize = ((BoxCollider2D) other).size;
 
@@ -48,15 +50,13 @@ public class PushOutEnemy : MonoBehaviour
             float halfSelfBoxLen = (selfBoxSize.x / 2.0f) * Math.Abs(self.transform.root.transform.localScale.x);
             float halfOtherBoxLen = (otherBoxSize.x / 2.0f) * Math.Abs(other.transform.root.transform.localScale.x);
             float pushBoxOverlap = (boxXDiff) - ((boxXDiff - halfOtherBoxLen) + (boxXDiff - halfSelfBoxLen));
-            Debug.Log(-pushBoxOverlap);
-            return new Vector3 (-pushBoxOverlap, 0f, 0f);
+            return new Vector3 (-pushBoxOverlap / 2.0f, 0f, 0f);
         } else {
             boxXDiff = selfPosition.x - otherPosition.x;
             float halfSelfBoxLen = (selfBoxSize.x / 2.0f) * Math.Abs(self.transform.root.transform.localScale.x);
             float halfOtherBoxLen = (otherBoxSize.x / 2.0f) * Math.Abs(other.transform.root.transform.localScale.x);
             float pushBoxOverlap = (boxXDiff) - ((boxXDiff - halfOtherBoxLen) + (boxXDiff - halfSelfBoxLen));
-            Debug.Log(pushBoxOverlap);
-            return new Vector3 (pushBoxOverlap, 0f, 0f);
+            return new Vector3 (pushBoxOverlap / 2.0f, 0f, 0f);
         }
     }
 }
